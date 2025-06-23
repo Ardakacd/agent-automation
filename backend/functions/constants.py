a082bab2ca59c17132d9f50ed91b144303687116
ardakabadayi <kabadayi_arda@hotmail.com>
2025-06-23 23:26:54 +0300

    add functions to test function call

---
+++ backend/functions/constants.py
@@ -0,0 +1,5 @@
+from .dummy_function import dummy_function
+
+FUNCTIONS = {
+    'Dummy Function': dummy_function
+}
---
+++ backend/functions/dummy_function.py
@@ -0,0 +1,2 @@
+def dummy_function(number: int, text: str) -> int:
+    return f'Here is the number: {number} and here is the text: {text}'
